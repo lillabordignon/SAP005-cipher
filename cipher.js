0d4dd62e4d4f1166d0ebd9dd07f5b36fa639bd1c
Lirian Bordignon <lirianbordignon@gmail.com>
2020-11-01 18:01:52 -0300

    Criação do JS

--- cipher.js
+++ cipher.js
@@ -1,5 +1,22 @@
-const cipher = {
-  // ...
-};
+const encode = (offset, message) => {
+    let alphabet = 26; 
+    let firstLetter = 65;
+    let result = '';
+    let convertResult;
+
+    for (let i = 0; i < message.length; i++){
+        let asciiNum = message.charCodeAt(i);
+        let displacement = (asciiNum - firstLetter + offset) % alphabet + firstLetter;
+        let convertString = String.fromCharCode(displacement);
+        result += convertString;
+    }
+    return result;
+}
+
+const decode = () => {
+    ;
+}
+
+const cipher = {encode, decode};
 
 export default cipher;
