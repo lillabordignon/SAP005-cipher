b6a8413ea962c3587f9f2c381f80489e1c90b928
Lirian Bordignon <lirianbordignon@gmail.com>
2020-11-01 18:01:52 -0300

    Criação do JS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+let elMessage = document.querySelector('#message');
+let elOffSet = document.querySelector('#offset');
+let result = document.querySelector('#result');
+
+const encode = () => {
+    let message = elMessage.value.toUpperCase();
+    let valOffSet = elOffSet.value;
+    let offSet = Number.parseInt(valOffSet);
+    let resultEncode = cipher.encode(offSet, message);
+    result.value = resultEncode;
+}
+
+const decode = () => {
+    let message = elMessage.value.toUpperCase();
+    let valOffSet = elOffSet.value;
+    let offSet = Number.parseInt(valOffSet);
+    let resultDecode = cipher.decode();
+    result.value = resultDecode;
+}
+
+const reset = () => {
+    document.querySelector('#message').value = '';
+    document.querySelector('#result').value = '';
+
+}
+
+let btnEncode = document.querySelector('#btnEncode');
+btnEncode.addEventListener('click', encode);
+let btnDecode = document.querySelector('#btnDecode');
+btnDecode.addEventListener('click', decode);
+let btnReset = document.querySelector('#btnReset');
+btnReset.addEventListener('click', reset);
